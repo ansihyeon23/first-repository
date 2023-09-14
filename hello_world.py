a89661f250b0cb3679fe0aed7f35ea69cda5b394
ansihyeon23 <ash040624@naver.com>
2023-09-14 14:01:17 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,8 @@
-def print_hello():
-    print("Hello World!!")
-
+def add(x, y):
+    a = x + y
+    return a
 def print_KIT():
-    print("Kumoh National Institute of technology.")
-if __name__ == '__main__':
-    print_KIT()
-    print_hello()
-    
+    print("KIT")
+
+if _name_ == '_main_':
+    print_KIT
